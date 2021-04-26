2c1863365bfcda0aaaf3ce2190c1fa4acfeb9f5d
yimi97 <msif1900@gmail.com>
2021-04-26 15:19:13 -0400

    add setup

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='sghmc',
+      version='0.1',
+      description='Implementation of Stochastic Gradient Hamiltonian Monte Carlo',
+      url='https://github.com/yimi97/sghmc',
+      author='Qinzhe Wang & Yi Mi',
+      author_email='qw92@duke.edu & yi.mi@duke.edu',
+      license='MIT',
+      packages=['sghmc'],
+      zip_safe=False)
